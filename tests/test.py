c37a1218d867a765c26e9fc901da93bc899e7473
18070949 <347246873@qq.com>
2022-04-05 16:37:14 +0800

    add dihn

---
+++ tests/test.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+#(3,2,3)
+t1 = [[[1, 1, 1],
+       [2, 2, 2]],
+      [[3, 3, 3],
+       [4, 4, 4]],
+      [[5, 5, 5],
+       [6, 6, 6]]]
+
+# t1 = tf.constant(t1)
+
+# sess = tf.Session()
+# result = sess.run(tf.reshape(t1, [2,-1]))
+# print(result)
+
+a = [1]
+b=[1,3,5,7]
+print( -2 ** 32 + 1)
+print(b[1:-1])
